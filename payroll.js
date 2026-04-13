c3beae0cf157ca4d15a66a8ea7f75cf0471704c4
101timx-source <101timx@gmail.com>
2026-04-13 12:26:35 -0400

    Add files via upload
    Made changes to the payroll script

--- payroll.js
+++ payroll.js
@@ -1,24 +1,30 @@
 var readLineSync = require('readline-sync');
-let employees = []; // array of objects
+let employees = []; 
+
 for (let i = 0; i < 3; i++) {
-console.log("\nEmployee #" + (i + 1));
+    console.log("\nEmployee #" + (i + 1));
 
-let name = readLineSync.question("Enter name: ");
-let wage;
-    do {
-        wage = readLineSync.questionFloat("Enter hourly wage: ");
-        if (wage <= 0) {
-            console.log("Wage must be positive!");
+    let name = readLineSync.question("Please Enter Full Legal name: ");
+
+    let wage;
+    while (true) {
+        wage = readLineSync.questionFloat("Please Enter Accurate Hourly Wage:");
+        if (wage > 0) {
+            break;
+        } else {
+            console.log("Please Enter a Positive Number");
         }
-    } while (wage <= 0);
-
+    }
 let hours;
-    do {
-        hours = readLineSync.questionFloat("Enter hours worked: ");
-        if (hours < 0 || hours > 80) {
-            console.log("Hours must be between 0 and 80!");
+    while (true) {
+        hours = readLineSync.questionFloat("Enter All Hours Worked Please:");
+        if (hours >= 0 && hours <= 80) {
+            break;
+        } else {
+            console.log("Please Enter Your Accurate Hours 0 through 80");
         }
-} while (hours < 0 || hours > 80);
+    }
+
 let regularHours = Math.min(hours, 40);
 let overtimeHours = Math.max(hours - 40, 0);
 let regularPay = regularHours * wage;
@@ -26,11 +32,11 @@ let overtimePay = overtimeHours * wage * 1.5;
 let totalPay = regularPay + overtimePay;
 
 employees.push({
-name: name,
-hours: hours,
-regularPay: regularPay,
-overtimePay: overtimePay,
-totalPay: totalPay
+    name: name,
+    hours: hours,
+    regularPay: regularPay,
+    overtimePay: overtimePay,
+    totalPay: totalPay
     });
 }
 
@@ -39,10 +45,11 @@ let highest = employees[0];
 for (let emp of employees) {
     if (emp.totalPay > highest.totalPay) {
         highest = emp;
+    } else {
     }
 }
 
-console.log("\n=== Payroll Report ===");
+console.log("Payroll Report For All Employees");
 
 for (let emp of employees) {
     console.log("\nName: " + emp.name);
@@ -51,4 +58,5 @@ for (let emp of employees) {
     console.log("Overtime Pay: $" + emp.overtimePay.toFixed(2));
     console.log("Total Pay: $" + emp.totalPay.toFixed(2));
 }
+
 console.log("\nHighest Paid Employee: " + highest.name + " ($" + highest.totalPay.toFixed(2) + ")");
